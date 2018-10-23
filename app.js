6680cb67c95601b3ea8ddc6fa9370cd6a8d5c630
Nick Kasten <kastentx@gmail.com>
2018-10-23 16:59:34 -0500

    refactor

--- app.js
+++ app.js
@@ -11,17 +11,11 @@ require('@tensorflow/tfjs-node-gpu')
 
 const MODEL_PATH = 'file://tensorflowjs_model.pb'
 const WEIGHTS_PATH = 'file://weights_manifest.json'
-let model
 
 const OBJ_LIST = ['background', 'airplane', 'bicycle', 'bird', 'boat', 
 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'dining table', 
 'dog', 'horse', 'motorbike', 'person', 'potted plant', 'sheep', 
 'sofa', 'train', 'tv']
-
-const loadTFJSModel = () => 
-  tf.loadFrozenModel(MODEL_PATH, WEIGHTS_PATH)
-
-//console.log(`using backend: ${ tf.getBackend() }`)
 
 if (filename) { 
   sharp(filename)
@@ -33,19 +27,13 @@ if (filename) {
       img.onload = async () => await ctx.drawImage(img, 0, 0)
       img.onerror = err => { throw err }
       img.src = data
-      const myTensor = tf.fromPixels(canvas).expandDims()
-      //console.log(myTensor)      
+      const myTensor = tf.fromPixels(canvas).expandDims()   
       try {
-        model = await loadTFJSModel()  
+        const model = await tf.loadFrozenModel(MODEL_PATH, WEIGHTS_PATH)
         const modelOutput = Array.from(model.predict(myTensor).dataSync())
         const objIDs = [...new Set(modelOutput)]
-
-        /* JACKPOT! */
-        
-        //console.log(`modelOutput: ${ modelOutput }`)
         const objTypes = objIDs.map(x => OBJ_LIST[x])
         console.log(`The image '${filename}' contains: ${objTypes.join(', ')}`)
-
       } catch (e) {
         console.error(`error loading model - ${ e }`)
       }
@@ -53,18 +41,4 @@ if (filename) {
 } else {
   console.error(`no input image specified.`)
   return
-}
-
-
-
-if (filename) {
-  //console.log(`submitted filename: ${ filename }`)
-  //console.log(`submitted image: ${ base64_encode(filename) }`)
-  //const base64 = base64_encode(filename)
-  //const loadedImage = fs.readFileSync(filename)
-  //const myTensor = tf.fromPixels(canvas)
-  //console.log(myTensor)
-  //console.log(canvas.toDataURL())
-  //const resizedTensor = tf.image.resizeBilinear(myTensor, [513, 513])
-  //console.log(resizedTensor)
 }
